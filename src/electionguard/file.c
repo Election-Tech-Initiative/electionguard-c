445773a4dcd7cf852e216554791373ccdd492b4f
Keith Fung <keithrfung@users.noreply.github.com>
2019-11-13 03:31:59 -0500

    🏗 Expose File Methods (#13)
    * 🏗 Expose File Methods
    
    Expose File methods to be used on external libraries. C File methods need to be exposed. The simple example file methods were used for simplicity.
    
    * 🏗 Add Seek File Method

---
+++ src/electionguard/file.c
@@ -0,0 +1,64 @@
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#ifdef _MSC_VER
+#include <io.h>
+#endif
+#include <electionguard/file.h>
+
+// Create a new file according to template
+FILE *File_new(char const *template)
+{
+    bool ok = true;
+    int result_fd = -1;
+    FILE *result = NULL;
+
+    #ifdef _WIN32
+        const char *mode = "w+";
+    #else
+        const char *mode = "w+x";
+    #endif
+
+    // Duplicate the template. It needs to be mutable for mkstemp.
+    char *template_mut = strdup(template);
+    if (template_mut == NULL)
+        ok = false;
+
+    // Create and open the temporary file
+    if (ok)
+    {
+        result_fd = mkstemp(template_mut);
+        if (-1 == result_fd)
+            ok = false;
+    }
+
+    // Convert the file descriptor to a FILE*
+    if (ok)
+    {
+        result = fdopen(result_fd, mode);
+        if (result == NULL)
+            ok = false;
+    }
+
+    // Free the duplicated template
+    if (template_mut != NULL)
+    {
+        free(template_mut);
+        template_mut = NULL;
+    }
+
+    return result;
+}
+
+// Close file
+void File_close(FILE *file)
+{
+    fclose(file);
+}
+
+// Seek file to beginning
+void File_seek(FILE *file)
+{
+    int seek_status = fseek(file, 0L, SEEK_SET);
+}

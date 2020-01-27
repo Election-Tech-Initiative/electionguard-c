190dfd2644185a4d2e565fe0e18cb3d8c5df5683
Matt <github@addressxception.com>
2020-01-27 12:26:02 -0500

    Add convenience logging

---
+++ src/electionguard/log.h
@@ -0,0 +1,32 @@
+#ifndef __LOG_H__
+#define __LOG_H__
+
+#include <stdarg.h>
+#include <stdio.h>
+
+static void log_stdout(const char *fmt, ...)
+{
+    va_list args;
+    va_start(args, fmt);
+    vprintf(fmt, args);
+    va_end(args);
+}
+
+static void log_stderr(const char *fmt, ...)
+{
+    va_list args;
+    va_start(args, fmt);
+    vfprintf(stderr, fmt, args);
+    va_end(args);
+}
+
+#ifdef DEBUG
+#define DEBUG_PRINT(x) do { log_stdout x; } while (0)
+#else
+#define DEBUG_PRINT(x) do {} while (0)
+#endif
+
+#define INFO_PRINT(x) do { log_stdout x; } while (0)
+#define ERROR_PRINT(x) do { log_stderr x; } while (0)
+
+#endif /* __LOG_H__ */
